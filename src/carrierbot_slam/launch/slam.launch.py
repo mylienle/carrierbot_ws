385a6dc9c450969aa0c06192022be083379cf06e
hcmutduygit <quangduy160204@gmail.com>
2026-07-04 14:54:08 +0700

    uncoment test map

--- src/carrierbot_slam/launch/slam.launch.py
+++ src/carrierbot_slam/launch/slam.launch.py
@@ -30,7 +30,8 @@ def generate_launch_description():
         get_package_share_directory("carrierbot_bringup"),
         "maps",
         # "carrierbot_slam.yaml"
-        "lab_map.yaml"
+        "testlab_map.yaml"
+        # "testb1_map.yaml"
     ])
     
     nav2_map_server = Node(
